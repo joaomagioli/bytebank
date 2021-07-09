dfcd396f46eb91b7728a9276555ffb87918011a6
João Magioli <joaoguilhermemagiolisouza@correio.itau.com.br>
2021-07-09 16:44:45 -0300

    Adding transaction to the project

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:bytebank/screens/contacts_list.dart';
+import 'package:bytebank/screens/transaction_list.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -15,36 +16,69 @@ class Dashboard extends StatelessWidget {
           Padding(
               padding: const EdgeInsets.all(8.0),
               child: Image.asset('images/bytebank_logo.png')),
-          Material(
-            color: Theme.of(context).primaryColor,
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => ContactList()));
-              },
-              child: Container(
-                padding: EdgeInsets.all(8.0),
-                height: 100,
-                width: 150,
-                color: Colors.green,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.people,
-                      color: Colors.white,
-                      size: 24.0,
-                    ),
-                    Text('Contacts',
-                        style: TextStyle(color: Colors.white, fontSize: 16.0))
-                  ],
-                ),
+          Row(
+            children: [
+              _FeatureItem(
+                "Transfer",
+                Icons.monetization_on,
+                () => _showContactsList(context),
               ),
-            ),
-          )
+              _FeatureItem("Transaction feed", Icons.description,
+                  () => _showTransactionList(context))
+            ],
+          ),
         ],
       ),
     );
   }
+}
+
+class _FeatureItem extends StatelessWidget {
+  final String featureName;
+  final IconData featureIcon;
+  final Function onClick;
+
+  _FeatureItem(this.featureName, this.featureIcon, this.onClick);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () => onClick(),
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            height: 100,
+            width: 150,
+            color: Colors.green,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(
+                  featureIcon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(featureName,
+                    style: TextStyle(color: Colors.white, fontSize: 16.0))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void _showContactsList(BuildContext context) {
+  Navigator.of(context)
+      .push(MaterialPageRoute(builder: (context) => ContactList()));
+}
+
+void _showTransactionList(BuildContext context) {
+  Navigator.of(context)
+      .push(MaterialPageRoute(builder: (context) => TransactionsList()));
 }
