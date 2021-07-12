b8fc957e4bcb9d35f4c8954e457928a6271e754e
João Magioli <joaoguilhermemagiolisouza@correio.itau.com.br>
2021-07-12 13:26:14 -0300

    Parsing the response

--- lib/http/web_client.dart
+++ lib/http/web_client.dart
@@ -1,14 +1,32 @@
+import 'dart:convert';
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
 // This method uses my local ip to fetch data.
 // The api was provided by Alura as a local project
-void getAllTransactions() async {
+Future<List<Transaction>> getAllTransactions() async {
   final Client client =
       InterceptedClient.build(interceptors: [LoggingInterceptor()]);
   final Response response =
       await client.get(Uri.parse('http://192.168.15.133:8080/transactions'));
+  final List<dynamic> decodedJson = jsonDecode(response.body);
+  final List<Transaction> transactions = [];
+  _handleResponse(decodedJson, transactions);
+  return transactions;
+}
+
+void _handleResponse(
+    List<dynamic> decodedJson, List<Transaction> transactions) {
+  for (Map<String, dynamic> element in decodedJson) {
+    final Transaction transaction = Transaction(
+        element['value'],
+        Contact(0, element['contact']['name'],
+            element['contact']['accountNumber']));
+    transactions.add(transaction);
+  }
 }
 
 class LoggingInterceptor implements InterceptorContract {
