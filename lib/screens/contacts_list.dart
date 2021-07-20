dcee9dbf19f8f4c4aed0133e0e7c6a1d63dad551
João Magioli <jmagioli@ciandt.com>
2021-07-19 22:19:27 -0300

    Implementing transaction form inside contacts list

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -2,6 +2,7 @@ import 'package:bytebank/components/Progress.dart';
 import 'package:bytebank/database/app_database.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
+import 'package:bytebank/screens/transaction_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactList extends StatefulWidget {
@@ -37,7 +38,13 @@ class _ContactListState extends State<ContactList> {
                     itemCount: contacts.length,
                     itemBuilder: (context, index) {
                       final Contact contact = contacts[index];
-                      return _ContactItem(contact);
+                      return _ContactItem(
+                        contact,
+                        onClick: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => TransactionForm(contact)));
+                        },
+                      );
                     },
                   );
                 }
@@ -63,13 +70,15 @@ class _ContactListState extends State<ContactList> {
 
 class _ContactItem extends StatelessWidget {
   final Contact contact;
+  final Function onClick;
 
-  _ContactItem(this.contact);
+  _ContactItem(this.contact, {required this.onClick});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
+        onTap: () => onClick(),
         title: Text(
           contact.name,
           style: TextStyle(fontSize: 24.0),
